781a0e4d266ff99bc42169eb4548d2bff7f09a29
Logan Zechella <jloganzechella@gmail.com>
2022-07-16 17:28:18 -0400

    Login window popup and close button JS added and working. Button animations added.

--- Scripts/landing-scripts.js
+++ Scripts/landing-scripts.js
@@ -1,9 +1,12 @@
-// Sign Up Button 
-
-$(".landing-button-signup").hover(function () {
-    $(this).toggleClass("is-active");
-});
 
 // Login Pop-up 
+function toggleLogin() {
+    document.getElementById("login-window").style.display = "flex";
+    document.getElementById("hero").style.display = "none";
+};
 
-
+// Close button
+function closeLoginWindow() {
+    document.getElementById("login-window").style.display = "none";
+    document.getElementById("hero").style.display = "flex";
+}
