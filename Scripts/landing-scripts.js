ce52d98dc96d2c4201fa95efe3bbea2ee014d9aa
Logan Zechella <jloganzechella@gmail.com>
2022-07-18 18:03:09 -0400

    Added about PRYSM and Founders section to landing page. WORK IN PROGRESS

--- Scripts/landing-scripts.js
+++ Scripts/landing-scripts.js
@@ -3,10 +3,16 @@
 function toggleLogin() {
     document.getElementById("login-window").style.display = "flex";
     document.getElementById("hero").style.display = "none";
+    document.getElementById("about-prysm").style.display = "none";
+    document.getElementById("login-btn").style.display = "none";
+    document.getElementById("founders").style.display = "none";
 };
 
 // Close button
 function closeLoginWindow() {
     document.getElementById("login-window").style.display = "none";
     document.getElementById("hero").style.display = "flex";
+    document.getElementById("about-prysm").style.display = "flex";
+    document.getElementById("login-btn").style.display = "block";
+    document.getElementById("founders").style.display = "flex";
 }
