c8e06f625b4c6c9ef9cdef809f17fa45a483da4e
Logan Zechella <jloganzechella@gmail.com>
2022-07-18 22:28:39 -0400

    New landing page background gradient. Added dropdown menu for mobile with animations and JS working. CSS media inquires also working.

--- Scripts/landing-scripts.js
+++ Scripts/landing-scripts.js
@@ -32,3 +32,13 @@ function closeLoginWindow() {
     document.getElementById("login-btn").style.display = "block";
     document.getElementById("founders").style.display = "flex";
 };
+
+// Hamburger Animations
+(function () {
+    var burger = document.querySelector('.burger-container'),
+        header = document.querySelector('.menu-container');
+
+    burger.onclick = function () {
+        header.classList.toggle('menu-opened');
+    }
+}());
